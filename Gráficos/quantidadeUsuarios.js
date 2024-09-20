34091bbc4504492d724325837ce2a525725d1fa4
Sebqstin <110260631+Sebqstin@users.noreply.github.com>
2024-09-20 11:40:12 -0300

    s

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Você sabe que morrem aproximadamente 100.000 pessoas diariamente?',
+            text: 'Você sabia que ass pessoas conectadas representam 67% da população mundial, ou seja 5,4 bilhões de pessoas?',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
