37c6c73e790482b6764dfe899afe0e40b76d1ce8
Sebqstin <110260631+Sebqstin@users.noreply.github.com>
2024-10-09 09:26:49 -0300

    ccc

---
+++ Gráficos/grafico-2.js
@@ -0,0 +1,59 @@
+import { getCSS, tickConfig } from "./common.js "
+
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = ["2021","2020","2019","2018","2017","2016"]
+    const quantidadeUsuarios = ["3.4","3.8","4.2","4.5","4.9","5.25"]
+    const data = [
+        {
+            x: nomeDasRedes,
+            y: quantidadeUsuarios,
+            type: 'bar',
+            marker: {
+                color: getCSS('--cor-barra')
+            }
+        }
+    ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--cor-de-fundo'),
+        paper_bgcolor: getCSS('--cor-de-fundo'),
+        title: {
+            text: 'Gráfico de pessoas conectadas à internet ao ano desde 2016 até 2021',
+            font: {
+                color: getCSS('--cor-principal'),
+                family: getCSS('--fonte'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Anos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de pessoas conectadas',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+
+        }
+
+    }
+    const grafico = document.createElement('div')
+    grafico.className - 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, layout)
+
+}
+
+quantidadeUsuarios()
