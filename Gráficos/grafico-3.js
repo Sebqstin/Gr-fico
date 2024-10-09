6cf61d09cb048fc38ba1df8b7e420ab6c878f061
Sebqstin <110260631+Sebqstin@users.noreply.github.com>
2024-10-09 09:51:07 -0300

    dasd

--- Gráficos/grafico-3.js
+++ Gráficos/grafico-3.js
@@ -4,8 +4,8 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = ["2021","2020","2019","2018","2017","2016"]
-    const quantidadeUsuarios = ["3.4","3.8","4.2","4.5","4.9","5.25"]
+    const nomeDasRedes = ["2023","2022","2021","2020","2019"]
+    const quantidadeUsuarios = ["62","61","61","60","59 "]
     const data = [
         {
             x: nomeDasRedes,
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'Gráfico de pessoas conectadas à internet ao ano desde 2016 até 2021',
+            text: 'Número de pessoas mortas desde o ano 2019 até 2023',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
@@ -40,7 +40,7 @@ async function quantidadeUsuarios() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Bilhões de pessoas conectadas',
+                text: 'Milhões de pessoas mortas',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
